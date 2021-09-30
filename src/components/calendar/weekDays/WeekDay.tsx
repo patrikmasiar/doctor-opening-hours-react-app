940db58cfe999ed418b92aa4b6885a7f76e41379
patrikmasiar <masiar.patrik@gmail.com>
2021-09-30 20:33:49 +0200

    Update style

--- src/components/calendar/weekDays/WeekDay.tsx
+++ src/components/calendar/weekDays/WeekDay.tsx
@@ -1,5 +1,6 @@
 import moment from 'moment';
 import { FC } from 'react';
+import style from 'components/calendar/weekDays/weekDay/WeekDay.module.scss';
 
 type Props = {
   date: string;
@@ -7,11 +8,16 @@ type Props = {
 
 const WeekDay: FC<Props> = ({ date }) => {
   return (
-    <div>
-      <div>
-        {moment(date).format('DD.MM.YYYY')}
+    <div className={style.item}>
+      <div className={style.info}>
+        <span>
+          {moment(date).format('DD.MM')}
+        </span>
+        <span>
+          {moment(date).format('YYYY')}
+        </span>
       </div>
-      <div>
+      <div className={style.terms}>
         terms
       </div>
     </div>
