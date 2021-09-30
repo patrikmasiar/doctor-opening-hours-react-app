7401e45aabd321b178b03f3dae6a16625d704f23
patrikmasiar <masiar.patrik@gmail.com>
2021-09-30 18:48:13 +0200

    Install ant design library

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'styles/index.scss';
+import 'antd/dist/antd.css';
 import App from 'components/App';
 import reportWebVitals from './reportWebVitals';
 
