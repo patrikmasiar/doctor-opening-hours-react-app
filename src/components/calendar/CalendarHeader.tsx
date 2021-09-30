06db9af3811e5884d9aeac0e05224dbcf12c0d31
patrikmasiar <masiar.patrik@gmail.com>
2021-09-30 22:32:08 +0200

    Addd tooltips to buttons

--- src/components/calendar/CalendarHeader.tsx
+++ src/components/calendar/CalendarHeader.tsx
@@ -1,4 +1,4 @@
-import { Button } from 'antd';
+import { Button, Tooltip } from 'antd';
 import { FC } from 'react';
 import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 import style from 'components/calendar/calendarHeader/CalendarHeader.module.scss';
@@ -13,18 +13,22 @@ const CalendarHeader: FC<Props> = ({ onPrevClick, onNextClick }) => {
     <div className={style.header}>
       <span>Header title</span>
       <div>
-        <Button
-          type="primary"
-          shape="circle"
-          onClick={onPrevClick}
-          icon={<LeftOutlined />}
-        />
-        <Button
-          type="primary"
-          shape="circle"
-          onClick={onNextClick}
-          icon={<RightOutlined />}
-        />
+        <Tooltip title="Previous week">
+          <Button
+            type="primary"
+            shape="circle"
+            onClick={onPrevClick}
+            icon={<LeftOutlined />}
+          />
+        </Tooltip>
+        <Tooltip title="Next week">
+          <Button
+            type="primary"
+            shape="circle"
+            onClick={onNextClick}
+            icon={<RightOutlined />}
+          />
+        </Tooltip>
       </div>
     </div>
   );
