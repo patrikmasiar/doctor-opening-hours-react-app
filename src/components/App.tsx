0536ec626c8cbaf8db4e5500869061cc476db13e
patrikmasiar <masiar.patrik@gmail.com>
2021-09-30 19:50:49 +0200

    Init layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,18 @@
+import { Layout } from 'antd';
 import { FC } from 'react';
+import Calendar from 'components/Calendar';
+
+const { Header, Content } = Layout;
 
 const App: FC = () => {
   return (
-    <div>
-      Application
-    </div>
+    <Layout>
+      <Header>Header</Header>
+      <Content style={{ padding: '50px' }}>
+        <Calendar />
+      </Content>
+      {/* <Footer>Footer</Footer> */}
+    </Layout>
   );
 };
 
