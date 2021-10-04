eb1b6f37dc26248dd2d2c5c5fb3769249094b583
patrikmasiar <masiar.patrik@gmail.com>
2021-10-04 14:10:15 +0200

    Remove unused functions

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,4 +1,4 @@
-import moment, { Moment } from 'moment';
+import moment from 'moment';
 
 const DAY_NAMES = [
   'monday',
@@ -18,19 +18,4 @@ export const getDaysInWeek = (week: number) => {
   return DAY_NAMES.map((day) =>
     moment().day(day).isoWeek(week).format('YYYY-MM-DD'),
   );
-};
-
-export const getRandomDate = (start: Moment, end: Moment) => {
-  const endTime = +moment(end);
-  const startTime = +moment(start);
-
-  if (startTime > endTime) {
-    throw new Error('Start date is after end date');
-  }
-
-  const randomNumber = Math.floor(
-    Math.random() * (endTime - startTime) + startTime,
-  );
-
-  return moment(randomNumber);
 };
