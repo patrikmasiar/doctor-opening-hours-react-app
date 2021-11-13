cc59fe4d7a0d01e251c84dc027e1847e15c9769b
patrikmasiar <masiar.patrik@gmail.com>
2021-11-13 12:38:31 +0100

    Add max day/week reservations count to config

--- src/components/reservationForm/ReservationForm.utils.ts
+++ src/components/reservationForm/ReservationForm.utils.ts
@@ -1,3 +1,4 @@
+import config from 'config';
 import moment, { Moment } from 'moment';
 import { Reservation } from 'store';
 
@@ -9,17 +10,19 @@ export const validateCreateReservation = (
   },
 ) => {
   if (
-    reservations.reservations.some((item) => reservation.date === item.date)
+    reservations.reservations.filter((item) => {
+      return reservation.date === item.date;
+    }).length === 1
   ) {
-    return 'You can not book more than 1 term per day.';
+    return `You can not book more than ${config.MAX_DAY_RESERVATIONS} term per day.`;
   }
 
   if (
     reservations.reservations.filter((item) => {
       return moment(item.date).isoWeek() === moment(reservation.date).isoWeek();
-    }).length === 2
+    }).length === config.MAX_WEEK_RESERVATIONS
   ) {
-    return 'You can not book more than 2 terms per week.';
+    return `You can not book more than ${config.MAX_WEEK_RESERVATIONS} terms per week.`;
   }
 
   if (
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 const config = {
   API_URL: process.env.REACT_APP_API_URL,
+  MAX_DAY_RESERVATIONS: 1,
+  MAX_WEEK_RESERVATIONS: 2,
 };
 
 export default config;
